05e2cf5e81f287f06a248f6996d3ed5f955a8db4
jainsenberg <110469451+jainsenberg@users.noreply.github.com>
2023-03-01 21:55:23 -0500

    comentarios

---
+++ MostazagramApp/src/Index/ContendorCardComment.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CardComment from './CardComment'
+
+const ContendorCardComment = () => {
+  return (
+    <div>
+        <CardComment/>
+        ContendorCardComment
+    </div>
+  )
+}
+
+export default ContendorCardComment
