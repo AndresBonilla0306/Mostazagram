07df98e335a0f707f3f2822b3b5a08180e6da374
Jainsenberg <juan_p.martinez_g@uao.edu.co>
2023-04-19 01:57:48 -0500

    login y register mas o menos buenos

--- MostazagramApp/src/Pages/Login.jsx
+++ MostazagramApp/src/Pages/Login.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/assets/img/Logo.png'
 
+
 const Login = () => {
+  
+  const [userData, setUserData] = useState({
+    usuario: "",
+    password: "",
+  });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    console.log("REGISTRADISIMO MI PAPA ")
+    console.log("*C VA AL LOGIN*")
+  };
+  
   return (
     <div className='Login'>
       <div className='ContenedorLogo'>
         <img src={Logo}></img>
       </div>
-      <div className='Form'>
-        <h1>Mostazagram</h1>
+      <form className='Form' onSubmit={handleSubmit}>
         <a>Usuario</a><br/>
-        <input type='text'></input><br/>
-        <a>Constraseña</a><br/>
-        <input type='text'></input><br/>
-        <button>Iniciar sesion</button><br/>
-        <button>¿No tienes cuenta?</button>
-      </div>
+          <input
+            type='text'
+            name="usuario"
+            value={userData.usuario}
+            onChange={handleInputChange}
+          /><br/>
+        <a>Contraseña</a><br/>
+          <input
+            type='text'
+            name="password"
+            value={userData.password}
+            onChange={handleInputChange}
+          /><br/>
+        <button type="submit">Registrarse</button>
+      </form>
     </div>
   )
 }
