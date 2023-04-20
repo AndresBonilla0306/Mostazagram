b8d2f03b17bf90202f017a73c4e0d2429d4398a0
Jainsenberg <juan_p.martinez_g@uao.edu.co>
2023-04-19 19:12:13 -0500

    LOGIN Y PUBLICAICONES FUNCIONALES SI EÑOR

--- MostazagramApp/src/Pages/Login.jsx
+++ MostazagramApp/src/Pages/Login.jsx
@@ -3,23 +3,63 @@ import Logo from '../assets/assets/img/Logo.png'
 
 
 const Login = () => {
-  
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+  );
   const [userData, setUserData] = useState({
     usuario: "",
     password: "",
   });
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+  
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+
+      } else {
+        setIsSubmitted(true);
+        
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+    
+    console.log("REGISTRADISIMO MI PAPA ")
+    console.log("C VA AL LOGIN")
+  };
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserData((prevUserData) => ({
       ...prevUserData,
       [name]: value,
     }));
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    
-    console.log("REGISTRADISIMO MI PAPA ")
-    console.log("*C VA AL LOGIN*")
   };
   
   return (
@@ -31,20 +71,19 @@ const Login = () => {
         <a>Usuario</a><br/>
           <input
             type='text'
-            name="usuario"
-            value={userData.usuario}
-            onChange={handleInputChange}
-          /><br/>
+            name="uname"
+          />
+          {renderErrorMessage("uname")}<br/>
         <a>Contraseña</a><br/>
           <input
             type='text'
-            name="password"
-            value={userData.password}
-            onChange={handleInputChange}
-          /><br/>
+            name="pass"
+          />
+          {renderErrorMessage("pass")}<br/>
         <button type="submit">Registrarse</button>
       </form>
     </div>
+    
   )
 }
 
