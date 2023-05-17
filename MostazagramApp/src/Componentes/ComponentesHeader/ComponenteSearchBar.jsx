bf570295e9e2908d05953c991fcbedea56648fd6
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-05-17 16:15:32 -0500

    Pruebita

--- MostazagramApp/src/Componentes/ComponentesHeader/ComponenteSearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComponenteSearchBar = () => {
-  return (
-    <div>ComponenteSearchBar</div>
-  )
-}
-
-export default ComponenteSearchBar
--- MostazagramApp/src/Index/ContenedorCardContact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ContenedorCardContact = () => {
-  return (
-    <div>ContenedorCardContact</div>
-  )
-}
-
-export default ContenedorCardContact
