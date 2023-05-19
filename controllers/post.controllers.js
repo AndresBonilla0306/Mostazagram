0f08b9753d4aa95dad1b61325793917b9c37ec35
Jainsenberg <juan_p.martinez_g@uao.edu.co>
2023-05-18 21:16:36 -0500

    cambios para conectar con frontend

--- controllers/post.controllers.js
+++ controllers/post.controllers.js
@@ -1,21 +1,25 @@
-import { request, response } from "express"
-import { postModel } from "../models/postModel.js"
+import { request, response } from "express";
+import { postModel } from "../models/postModel.js";
 
-const getPost = (request, response) => {
-    response.send("Ey muy buenas a todos aqui Willyrex")
-}
+const getPost = async (req, res) => {
+  try {
+    const posts = await postModel.find({});
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(502).json({ msg: "Pero madre mía Willy" });
+  }
+};
 const createPost = async (req = request, res = response) => {
-    const { photo, desc, user } = req.body
-    try {
+  const { photo, desc, user } = req.body;
+  try {
+    const newPost = new postModel({ photo, desc, user });
+    await newPost.save();
+    return res.json({ msg: newPost });
+  } catch (error) {
+    console.log(error);
+    res.status(502).json({ msg: "Pero madre mía Willy" });
+  }
+};
 
-        const newPost = new postModel({ photo, desc, user })
-        await newPost.save()
-        return res.json({ msg: newPost})
-
-    } catch (error) {
-        console.log(error)
-        res.status(502).json({ msg: "Pero madre mía Willy" })
-    }
-}
-
-export { getPost, createPost }
+export { getPost, createPost };
--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-import express from "express"
+import express from "express";
 import * as dotenv from "dotenv";
 import { DBconnection } from "./database/configmongodb.js";
-import users from "./routes/users.routes.js"
-import post from "./routes/post.routes.js"
-import comment from "./routes/comment.routes.js"
+import users from "./routes/users.routes.js";
+import post from "./routes/post.routes.js";
+import comment from "./routes/comment.routes.js";
+import cors from "cors";
 
 dotenv.config();
 DBconnection();
 
-
 const app = express();
 app.use(express.json());
-app.use("/api/users", users)
-app.use("/api/post", post)
-app.use("/api/comment", comment)
-app.listen(process.env.PORT, ()=> console.log("Salchipapa " + process.env.PORT) )
+app.use(cors());
+app.use("/api/users", users);
+app.use("/api/post", post);
+app.use("/api/comment", comment);
+app.listen(process.env.PORT, () =>
+  console.log("Salchipapa " + process.env.PORT)
+);
