1e5ff4d23a9342f6ea18d94eb67d423f30937846
Juan Pablo Martinez <juan_p.martinez_g@uao.edu.co>
2023-05-25 15:22:44 -0500

    cambito chikito

---
+++ src/Componentes/UImessage.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+
+
+
+const UlMensajes = styled.ul`
+
+    max-width: 800px;
+
+    margin: 10px auto;
+
+    list-style: none;
+
+    display: flex;
+
+    flex-direction: column;
+
+    gap: 5px;
+
+`;
+
+
+
+
+// const LiMensaje = styled.li`
+
+//     background-color: #74FFA8;
+
+//     border: 2px solid dodgerblue;
+
+//     padding: 10px 20px;
+
+// `
+
+// const LiMensajeClient = styled.li`
+
+//     background-color: #A774FF;
+
+//     border: 2px solid dodgerblue;
+
+//     padding: 10px 20px;
+
+// `
+
+
+export {
+
+    UlMensajes
+
+}
