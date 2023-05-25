77464921067a434dc5fe0bb5ece9390d2d81195c
Juan Pablo Martinez <juan_p.martinez_g@uao.edu.co>
2023-05-25 15:19:52 -0500

    cambios para sockets en el chat

--- index.js
+++ index.js
@@ -5,16 +5,36 @@ import user from "./routes/users.routes.js";
 import post from "./routes/post.routes.js";
 import comment from "./routes/comment.routes.js";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 
 dotenv.config();
 DBconnection();
 
 const app = express();
+
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { 
+  cors: {origin: '*'}
+ });
+
+io.on("connection", (socket) => {
+  console.log('se conecto tu papa');
+  socket.on('chat_message', (data)=>{
+    console.log(data)
+    io.emit('chat_message', data);
+  })
+});
+
 app.use(express.json());
 app.use(cors());
 app.use("/api/user", user);
 app.use("/api/post", post);
 app.use("/api/comment", comment);
-app.listen(process.env.PORT, () =>
-  console.log("Salchipapa " + process.env.PORT)
-);
+// app.listen(process.env.PORT, () =>
+//   console.log("Salchipapa " + process.env.PORT)
+// );
+
+httpServer.listen(process.env.PORT);
