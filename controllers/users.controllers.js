d299b22063d38eec8f8eb72b4e342fc6822fd017
AndresBonilla0306 <andres.bonilla@uao.edu.co>
2023-05-26 15:46:16 -0500

    Login

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -21,6 +21,41 @@ const createUser = async (req = request, res = response) => {
     res.status(502).json({ msg: "algo" });
   }
 };
-const loginUsuario = async (req, res = express.request) => {};
-const revalidarToken = async (req, res = express.request) => {};
+const loginUsuario = async (req, res = express.request) => {
+  const { user, pass } = req.body;
+
+  try {
+    let usuario = await userModel.findOne({ user: user });
+    if (!usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El usuario NO existe en la DB",
+      });
+    }
+
+    const passwordValid = bcrypt.compareSync(pass, usuario.pass);
+    if (!passwordValid) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El password NO es válido",
+      });
+    }
+
+    const token = await generarJWT(usuario.id, usuario.name);
+
+    res.status(200).json({
+      ok: true,
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      error,
+    });
+  }
+};
+const revalidarToken = async (req, res = express.request) => {
+};
 export { getUser, createUser, loginUsuario, revalidarToken };
