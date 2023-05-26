5f6f80275fc78b9c077842e2c00676b948f0af22
Jainsenberg <juan_p.martinez_g@uao.edu.co>
2023-05-26 13:53:14 -0500

    Tokens

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createUser, getUser } from "../controllers/users.controllers.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { validateUser } from "../validators/checks.js";
+import { validarJWT } from "../middlewares/validar-token.js";
 
 const router = express.Router();
 
