50dc47b1f4217005e3ae852063ca9fd903c8e880
Jainsenberg <juan_p.martinez_g@uao.edu.co>
2023-05-29 10:44:22 -0500

    Comentarios 100% funcionales

--- models/postModel.js
+++ models/postModel.js
@@ -6,7 +6,7 @@ export const postSchema = Schema({
   photo: { type: String, required: true },
   desc: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  comment: [{ type: Schema.Types.ObjectId, ref: "comment", required: true }],
+  comment: [{ type: Schema.Types.ObjectId, ref: "comment", required: false }],
 });
 
 const postModel = model("Posts", postSchema);
